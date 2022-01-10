09d2206fa7911a2d0a0fcfa697489234c32c8fd4
Joona Kauppila <joona.kauppila@helsinki.fi>
2022-01-10 08:02:13 +0200

    5.9 Sort blogs by likes

--- bloglist-frontend/src/components/BlogList.js
+++ bloglist-frontend/src/components/BlogList.js
@@ -4,9 +4,9 @@ import Blog from './Blog'
 const BlogList = ({ blogs, likeBlog }) => {
   return (
     <div>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} likeBlog={likeBlog} />
-      )}
+      {blogs
+        .sort((a, b) => a.likes <= b.likes ? 1 : -1)
+        .map(blog => <Blog key={blog.id} blog={blog} likeBlog={likeBlog} />)}
     </div>
   )
 }
